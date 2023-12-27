88bee4a127d6d522cedfd9d996d9fa5a0abdadb1
Jain K <kuriakosejain456@gmail.com>
2023-12-27 19:11:23 +0530

    added firebase sdk

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCg5BRWYrcAQ0NSQBU4ykp16iHPxSkARSw",
+  authDomain: "react-notes-4ad0b.firebaseapp.com",
+  projectId: "react-notes-4ad0b",
+  storageBucket: "react-notes-4ad0b.appspot.com",
+  messagingSenderId: "87774406925",
+  appId: "1:87774406925:web:4d8170843dbbb8202c2844"
+};
+
+
+const app = initializeApp(firebaseConfig);
